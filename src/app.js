8a4d66f8d5044a6ccf4653688370d03dba6153e8
Danny Carcamo <dannycarcamo@icloud.com>
2021-01-22 11:55:12 -0600

    added logger and API key handling middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-'use strict';
+/* eslint-disable strict */
+/* eslint-disable no-console */
 
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('./logger');
 const { NODE_ENV } = require('./config');
 
 const app = express();
@@ -15,6 +17,18 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
@@ -30,6 +44,6 @@ app.use(function errorHandler(error, req, res, next) {
   }
   res.status(500).json(response);
 });
-app.use(cors());
+
 
 module.exports = app;
