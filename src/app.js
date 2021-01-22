43fbbe0237db9055caf4b20e2ce810eab6ff3179
Danny Carcamo <dannycarcamo@icloud.com>
2021-01-22 12:27:29 -0600

    refactored validateBearerToken and error-handler

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const logger = require('./logger');
 const { NODE_ENV } = require('./config');
+const bookmarksRouter = require('./bookmarks/bookmarks-router');
+const validateBearerToken = require('./validateBearerToken');
+const errorHandler = require('./error-handler');
 
 const app = express();
 
@@ -19,31 +21,14 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
+app.use(validateBearerToken);
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-  next();
-});
+app.use(bookmarksRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
-
+app.use(errorHandler);
 
 module.exports = app;
