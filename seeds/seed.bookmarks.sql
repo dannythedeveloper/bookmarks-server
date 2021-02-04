ab3044e55532023646f27c5de9a8733ec226a6e8
Danny Carcamo <dannycarcamo@icloud.com>
2021-02-04 12:41:16 -0600

    refactored GET /bookmarks and GET /bookmarks/:bookmark_id

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,13 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+	('Google', 'www.google.com', 'Description goes here', 5),
+	('Yahoo', 'www.yahoo.com', 'Description goes here', 4),
+	('MySpace', 'www.myspace.com', 'Description goes here', 5),
+	('Facebook', 'www.facebook.com', 'Description goes here', 3),
+	('Thinkful', 'www.thinkful.com', 'Description goes here', 5),
+	('Google', 'www.google.com', 'Description goes here', 5),
+	('Google', 'www.google.com', 'Description goes here', 5),
+	('Google', 'www.google.com', 'Description goes here', 5),
+	('Google', 'www.google.com', 'Description goes here', 5),
+	('Google', 'www.google.com', 'Description goes here', 5);
+	
